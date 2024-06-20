d7ba331f2981cd5597b24f9e8a74276c92947038
JosePereira1ciencia pereira <guilherme.cunhapereiraa@hotmail.com>
2024-06-20 02:27:22 +0000

    adicionada a função de validader a resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -99,3 +99,15 @@ async function iniciar() {
 }
 
 iniciar()
+function validarResposta() {
+    if (resposta === quiz.question[pergunta-1].answer){
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+         pontos = pontos + 1
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")   
+    }
+}
+if (input.value === quiz.question[pergunta-1].answer) {
+           respostaCorretaId = input.id
+        }
