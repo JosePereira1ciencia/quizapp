cadb3bb1b4de469e1b56cda83f5bf07804f7434d
JosePereira1ciencia pereira <guilherme.cunhapereiraa@hotmail.com>
2024-06-20 02:39:12 +0000

    finalizando quiz

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
@@ -7,6 +6,7 @@ const body = document.querySelector("body")
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema);
 })
+
 verificarTema(body, botaoTema);
 
 const botoesAssunto = document.querySelectorAll(".assuntos button");
